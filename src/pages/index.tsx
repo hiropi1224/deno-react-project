426cf9946874852d5c46a92f59011e650b224959
hiropi1224 <64931333+hiropi1224@users.noreply.github.com>
2025-05-04 15:29:16 +0900

    ✨ add tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,20 +14,27 @@ export default function Index() {
   }, []);
 
   return (
-    <main>
-      <h1>Welcome to the Dinosaur app</h1>
-      <p>Click on a dinosaur below to learn more.</p>
-      {dinosaurs.map((dinosaur: Dino) => {
-        return (
-          <Link
-            to={`/${dinosaur.name.toLowerCase()}`}
-            key={dinosaur.name}
-            className="dinosaur"
-          >
-            {dinosaur.name}
-          </Link>
-        );
-      })}
+    <main className="flex flex-col items-center justify-center bg-amber-50 size-full">
+      <h1 className="text-4xl">Welcome to the Dinosaur app</h1>
+      <p className="">Click on a dinosaur below to learn more.</p>
+      <ul className="grid grid-cols-4 gap-4 mt-4">
+        {dinosaurs.map((dinosaur: Dino) => {
+          return (
+            <li
+              key={dinosaur.name}
+              className="bg-amber-200 p-4 rounded-lg shadow-md hover:shadow-lg hover:bg-amber-300 transition-shadow"
+            >
+              <Link
+                to={`/${dinosaur.name.toLowerCase()}`}
+                key={dinosaur.name}
+                className=""
+              >
+                {dinosaur.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
