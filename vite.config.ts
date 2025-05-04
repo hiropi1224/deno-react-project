80c9b5101497ae07ee6f827e17d2145866b8306a
hiropi1224 <64931333+hiropi1224@users.noreply.github.com>
2025-05-04 14:55:25 +0900

    ✨ deno tutorial

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [deno(), react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+    },
+  },
 });
